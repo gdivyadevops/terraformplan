edd8eabb623aadda006eacca4ec9c28ef959359c
gdivyadevops <106365002+gdivyadevops@users.noreply.github.com>
2023-07-27 09:18:55 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+region     = "ap-south-1"
+}
+
+resource "aws_instance" "web-server" {
+ami           = "ami-008b85aa3ff5c1b02"
+instance_type = "t2.micro"
+
+  tags {
+    Name = "terraform-jenkins12"
+  }
+}
